d059ff9fd9dbfbc4926271897c80eb7c5edd4f07
Mintra Phophaijit <bluembc@gmail.com>
2019-11-30 22:06:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,4 @@
-def roll_call_dwarves["Doc", "Dopey", "Bashful", "Grumpy"]
- roll_call_dwarves.each do |name|
-  puts name
-end
+
 
 dwarves = %w[
   Doc
@@ -19,29 +16,9 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-roll_call_dwarves(dwarves)
-
-planeteer_calls = %w[earth wind fire water heart]
-
-# Expected return value: ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-summon_captain_planet(planeteer_calls)
-
-
-
-def find_the_cheese(cheese_types)
-  i = 0 
-  while i < cheese_types.length 
-  returns cheese_types[i] if yeild(cheese_types[i])
-  i = i +1 
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
-snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
-
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-find_the_cheese(soup)
+long_planeteer_calls(planeteer_calls)
 
