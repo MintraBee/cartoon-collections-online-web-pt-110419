71ae55d4414921d0fd32515b7b3948cad686481b
Mintra Phophaijit <bluembc@gmail.com>
2019-11-30 22:07:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,12 @@ dwarves = %w[
   Happy
 ]
 
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end
+
+summon_captain_planet(planeteer_calls)
+
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
